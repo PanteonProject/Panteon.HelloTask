648086a605948ee7290d54fed44822727afa3d97
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-08-27 12:40:46 +0300

    Add autoRun param to task Bootstrap method

--- HelloTask/HelloTask.cs
+++ HelloTask/HelloTask.cs
@@ -16,7 +16,7 @@ namespace Panteon.HelloTask
 
         public override string Name => "My-Hello-Task";
 
-        public override bool Bootstrap()
+        public override bool Bootstrap(bool autoRun)
         {
             return Run((task, offset) => DoSomething());
         }
