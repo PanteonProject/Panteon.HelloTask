a4455e161a556fccf6353c7eaddee8410941c353
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-09-04 09:55:08 +0300

    refactor configuration

--- HelloTask/Configuration/HelloTaskConfigProvider.cs
+++ HelloTask/Configuration/HelloTaskConfigProvider.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using System.Reflection;
-using Autofac.Extras.NLog;
+﻿using Autofac.Extras.NLog;
 using Panteon.Sdk.Configuration;
 using Panteon.Sdk.IO;
 using Panteon.Sdk.Utils;
@@ -9,32 +7,9 @@ namespace Panteon.HelloTask.Configuration
 {
     public class HelloTaskConfigProvider : TaskConfigProviderBase<HelloTaskSettings>
     {
-        private readonly IJsonSerializer _jsonSerializer;
-        private readonly IFileReader _fileReader;
-
         public HelloTaskConfigProvider(ILogger logger, IJsonSerializer jsonSerializer, IFileReader fileReader)
-            : base(logger)
+            : base(logger,jsonSerializer,fileReader)
         {
-            _jsonSerializer = jsonSerializer;
-            _fileReader = fileReader;
-        }
-
-        public override HelloTaskSettings ParseSettings(string configFilePath = null)
-        {
-            string asmLocation = Assembly.GetExecutingAssembly().Location;
-
-            string directoryName = Path.GetDirectoryName(asmLocation);
-
-            if (!string.IsNullOrEmpty(directoryName))
-            {
-                configFilePath = configFilePath ?? Path.Combine(directoryName, "config.json");
-            }
-
-            FileContentResult result = _fileReader.ReadFileContent(configFilePath);
-
-            HelloTaskSettings settings = _jsonSerializer.Deserialize<HelloTaskSettings>(result.Content);
-
-            return settings;
         }
     }
 }
