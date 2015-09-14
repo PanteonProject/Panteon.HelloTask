e027bca0de797e14f3dd5c7ab33533ccafb5e13c
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-09-14 14:43:13 +0300

    fix config load

--- HelloTask/Configuration/HelloTaskConfigProvider.cs
+++ HelloTask/Configuration/HelloTaskConfigProvider.cs
@@ -1,4 +1,6 @@
-﻿using Autofac.Extras.NLog;
+﻿using System.IO;
+using System.Reflection;
+using Autofac.Extras.NLog;
 using Panteon.Sdk.Configuration;
 using Panteon.Sdk.IO;
 using Panteon.Sdk.Utils;
@@ -7,9 +9,23 @@ namespace Panteon.HelloTask.Configuration
 {
     public class HelloTaskConfigProvider : TaskConfigProviderBase<HelloTaskSettings>
     {
+        private readonly IJsonSerializer _jsonSerializer;
+        private readonly IFileReader _fileReader;
+
         public HelloTaskConfigProvider(ILogger logger, IJsonSerializer jsonSerializer, IFileReader fileReader)
             : base(logger,jsonSerializer,fileReader)
         {
+            _jsonSerializer = jsonSerializer;
+            _fileReader = fileReader;
+        }
+
+        public override HelloTaskSettings ParseSettings(string configFilePath = null)
+        {
+            string directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (!string.IsNullOrEmpty(directoryName))
+                configFilePath = configFilePath ?? Path.Combine(directoryName, "config.json");
+
+            return _jsonSerializer.Deserialize<HelloTaskSettings>(_fileReader.ReadFileContent(configFilePath).Content);
         }
     }
 }
